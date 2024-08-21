cc1d163cf8a55e9f504a50920ee7c636c5352519
Michael Haken <michael.haken@outlook.com>
2024-08-21 19:29:52 -0400

    update

--- src/utilities/AvailabilityMetricType.ts
+++ src/utilities/AvailabilityMetricType.ts
@@ -2,30 +2,30 @@
  * Different availability metric types
  */
 export enum AvailabilityMetricType
-  {
+{
   /**
-     * The success rate, i.e. (successful responses) / (successful + fault responses) * 100
-     */
+   * The success rate, i.e. (successful responses) / (successful + fault responses) * 100
+   */
   SUCCESS_RATE = 'Success_Rate',
 
   /**
-     * The number of success responses as an absolute value
-     */
+   * The number of success responses as an absolute value
+   */
   SUCCESS_COUNT = 'Success_Count',
 
   /**
-     * The fault rate, i.e. (fault responses) / (successful + fault responses) * 100
-     */
+   * The fault rate, i.e. (fault responses) / (successful + fault responses) * 100
+   */
   FAULT_RATE = 'Fault_Rate',
 
   /**
-     * The number of fault responses as an absolute value
-     */
+   * The number of fault responses as an absolute value
+   */
   FAULT_COUNT = 'Fault_Count',
 
   /**
-     * The number of requests received that resulted in either a fault or success. This
-     * does not include "error" responses that would be equivalent to 4xx responses.
-     */
+   * The number of requests received that resulted in either a fault or success. This
+   * does not include "error" responses that would be equivalent to 4xx responses.
+   */
   REQUEST_COUNT = 'Request_Count'
 }
--- src/utilities/LatencyMetricType.ts
+++ src/utilities/LatencyMetricType.ts
@@ -2,14 +2,14 @@
  * The type of latency metric
  */
 export enum LatencyMetricType
-  {
+{
   /**
-     * Successful response latency
-     */
+   * Successful response latency
+   */
   SUCCESS_LATENCY = 'Success_Latency',
 
   /**
-     * Fault response latency
-     */
+   * Fault response latency
+   */
   FAULT_LATENCY = 'Fault_Latency'
 }
