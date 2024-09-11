4c493a7ddf98db686cb1baf7b2df98c241b173d6
Michael Haken <michael.haken@outlook.com>
2024-09-11 09:01:58 -0400

    updated formatting

--- .projenrc.ts
+++ .projenrc.ts
@@ -225,13 +225,12 @@ project.tasks.tryFind("post-compile")?.exec("npx awslint");
 
 // tsconfig.json gets the exclude list updated and isn't tracked
 project.tasks.tryFind("release")?.updateStep(4, {
-  exec: "git diff --ignore-space-at-eol --exit-code ':!tsconfig.json' ':!.projenrc.ts' ':!API.md'",
+  exec: "git diff --ignore-space-at-eol --exit-code ':!tsconfig.json'",
 });
 
 /*project.addFields({
   version: '0.0.1-alpha.1',
 });*/
-
 
 project.github
   ?.tryFindWorkflow("release")
